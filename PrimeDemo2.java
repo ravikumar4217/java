68e139d95e4f97dadbd8dab1950cc22b960a1bad
ravikumar4217 <143474137+ravikumar4217@users.noreply.github.com>
2023-09-04 11:53:37 +0530

    Create PrimeDemo2.java

---
+++ PrimeDemo2.java
@@ -0,0 +1,24 @@
+//java program to find wether the given number is prime or not.
+import java.util.*;
+public class PrimeDemo2
+{
+public static void main(String args[]) {
+    int i,num,flag=0;
+    System.out.println("Enter an integer to check prime or not : ");
+    Scanner sc = new Scanner(System.in);
+    num = sc.nextInt();
+    for(i=2;i<=num;i++)
+    {
+        if(num%i==0)
+        {
+            System.out.println(num+" is a prime number");
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0)
+    {
+        System.out.println(num+" is not a prime number");
+    }
+}
+}
