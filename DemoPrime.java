957dbd95419f5944c9b17ad4467fe377b3b0c544
ravikumar4217 <143474137+ravikumar4217@users.noreply.github.com>
2023-09-04 11:39:07 +0530

    Create DemoPrime.java

---
+++ DemoPrime.java
@@ -0,0 +1,21 @@
+//java program to find wether the given number is prime or not.
+public class DemoPrime
+{
+public static void main(String args[]) {
+    int num=44;
+    int flag=0;
+    for(int i=2;i<=num;i++)
+    {
+        if(num%i==0)
+        {
+            System.out.println(num+" is not a prime number");
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0)
+    {
+        System.out.println(num+" is a prime number");
+    }
+}
+}
