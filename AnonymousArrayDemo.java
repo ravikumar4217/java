29a77c95ebf48db44c3806718b8ce6f866005993
ravikumar4217 <143474137+ravikumar4217@users.noreply.github.com>
2023-10-03 09:00:42 +0530

    Create AnonymousArrayDemo.java

---
+++ AnonymousArrayDemo.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class AnonymousArrayDemo {
+     static void printArray(int arr[]) {
+         for(int i=0;i<arr.length;i++) {
+             System.out.println(arr[i]);
+         }
+     }
+     public static void main(String args[]) {
+         System.out.println("The array elements are : ")
+         printArray(new int[]{11,22,33,44,55});
+     }
+}
