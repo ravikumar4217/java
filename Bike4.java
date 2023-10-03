37bb181afa0665745ec1c7a185df5be9b7582352
ravikumar4217 <143474137+ravikumar4217@users.noreply.github.com>
2023-10-03 09:30:28 +0530

    Create Bike4.java

---
+++ Bike4.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class Bike4 {
+	final int speedlimit;
+	Bike4(){
+		speedlimit=40;
+		System.out.println("speed limit is "+speedlimit);
+		}
+	public static void main(String args[]){
+		Bike4 b=new Bike4();
+		
+	}
+}
