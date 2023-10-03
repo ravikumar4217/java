c7d8d62412b15579fb9385e5fb1a01e3510241a4
ravikumar4217 <143474137+ravikumar4217@users.noreply.github.com>
2023-10-03 09:51:18 +0530

    Create ArrayPassingDemo.java

---
+++ ArrayPassingDemo.java
@@ -0,0 +1,18 @@
+import java.io.*;
+public class ArrayPassingDemo {
+    static void min(int arr[]) {
+        int m = arr[0];
+        for(int i=0;i<arr.length;i++)
+        {
+            if(m>arr[i]){
+                m = arr[i];
+            }
+        }
+        System.out.println("The smallest element is "+m);
+    }
+    public static void main(String args[])
+    {
+        int a[] = {11,22,33,44,2,1,5,7};
+        min(a);
+    }
+}
