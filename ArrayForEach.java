750aa3946cc82a396f9fc6c77b06173929a1b795
ravikumar4217 <143474137+ravikumar4217@users.noreply.github.com>
2023-10-03 09:28:20 +0530

    Create ArrayForEach.java

---
+++ ArrayForEach.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class ArrayForEach{
+	public static void main(String args[]){
+		int a[]={11,22,33,44,55};
+		System.out.println("The array elements are ");
+		for(int i:a)
+		{
+			System.out.println(i+" ");
+	    }
+	  }
+    }
+		
