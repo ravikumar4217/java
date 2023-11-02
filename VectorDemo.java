aaac0dacf82f992babdce965c5bb72f0338da642
ravikumar4217 <143474137+ravikumar4217@users.noreply.github.com>
2023-11-02 15:36:46 +0530

    Create VectorDemo.java

---
+++ VectorDemo.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class VectorDemo{
+    public static void main(String args[]){
+        Vector<String>vec=new Vector<String>();
+        vec.add("UMME");
+        vec.add("20");
+        vec.add("Ravi");
+        System.out.println("The elements in the vector are : "+vec);
+    }
+}
